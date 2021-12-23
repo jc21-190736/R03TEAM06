942836554c0e2aed897bde6999f1700771e45466
柊斗 金賀 <190666@jc-21.jp>
2021-12-23 16:47:26 +0900

    追加画面

--- src/Tuika.java
+++ src/Tuika.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-//データベース追加用サーブレット
+//データベース追加用サーブレット　
 
 @WebServlet("/tuika")
 
