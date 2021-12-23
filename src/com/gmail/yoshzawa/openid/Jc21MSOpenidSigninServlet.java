2f6f4a41e32e0e5507cd3d8c34c5fae0cde8c116
yoshzawa <yoshzawa@gmail.com>
2021-12-23 16:14:11 +0900

    initial commit with microsoft login

---
+++ src/com/gmail/yoshzawa/openid/Jc21MSOpenidSigninServlet.java
@@ -0,0 +1,68 @@
+package com.gmail.yoshzawa.openid;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = { "/openidSignIn" })
+
+public final class Jc21MSOpenidSigninServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+		String url = null;
+		String localAddr = req.getLocalAddr();
+		if(localAddr.substring(0,3).contentEquals("192")) {
+			// Production
+			url = getProductionLoginUrl();
+		} else {
+			// Local development server
+			url = getDevelopLoginUrl();
+
+		}
+
+		if (url != null) {
+			resp.sendRedirect(url);
+		} else {
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp5/property/propertyError.jsp");
+			rd.forward(req, resp);
+		}
+
+	}
+
+	private String getDevelopLoginUrl() {
+		String url = null;
+		String AzureAppIdLocal = "a93c6516-b7b3-407e-b7ca-2dbc3d55e8c0";
+		if (AzureAppIdLocal != null) {
+			url = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize";
+			url += "?client_id=" + AzureAppIdLocal;
+			url += "&response_type=id_token";
+			url += "&redirec_uri=http%3A%2F%2Flocalhost%3A8080%2FloginSample%2Fmsredirect";
+			url += "&response_mode=form_post";
+			url += "&scope=openid%20profile";
+			url += "&state=12345";
+			url += "&nonce=678910";
+		}
+		return url;
+	}
+
+	private String getProductionLoginUrl() {
+		String url = null;
+		String AzureAppId = null;
+		if (AzureAppId != null) {
+			url = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize";
+			url += "?client_id=" + AzureAppId;
+			url += "&response_type=id_token";
+			url += "&redirec_uri=https%3A%2F%2Ffegogo.fivepro.xyz%2Fmsredirect";
+			url += "&response_mode=form_post";
+			url += "&scope=openid%20profile";
+			url += "&state=12345";
+			url += "&nonce=678910";
+		}
+		return url;
+	}
+}
